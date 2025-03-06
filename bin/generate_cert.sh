f13e156004192684966e1bbe702f45e11debafd7
Robert <robert@webtree>
2025-03-06 23:20:55 +0100

    update compose incl. selfsigned cert

--- bin/generate_cert.sh
+++ bin/generate_cert.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# Extract domain from BASE_URL
+DOMAIN=$(echo $BASE_URL | sed 's/^https\?:\/\///' | sed 's/\/$//')
+
 CERT_DIR="/certs"
-DOMAIN="tree.klaus-fam.de"
+CERT_KEY="$CERT_DIR/webtrees.key"
+CERT_CRT="$CERT_DIR/webtrees.crt"
 VALIDITY_DAYS=3650  # 10 years in days
 
-# Generate the self-signed certificate
-#openssl req -newkey rsa:2048 -days $VALIDITY_DAYS -nodes -x509 \
-#  -keyout "$CERT_DIR/tree.klaus-fam.de.key" \
-#  -out "$CERT_DIR/tree.klaus-fam.de.crt" \
-#  -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"
+# Check if the certificate and key already exist
+if [ -f "$CERT_KEY" ] && [ -f "$CERT_CRT" ]; then
+  echo "Certificate already exists. Skipping creation."
+else
+  # Generate the self-signed certificate if it doesn't exist
+  echo "Generating self-signed certificate for $DOMAIN..."
+  openssl req -newkey rsa:2048 -days $VALIDITY_DAYS -nodes -x509 \
+    -keyout "$CERT_KEY" -out "$CERT_CRT" \
+    -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"
 
-openssl req -newkey rsa:2048 -days $VALIDITY_DAYS -nodes -x509 \
-  -keyout "$CERT_DIR/webtrees.key" \
-  -out "$CERT_DIR/webtrees.crt" \
-  -subj "/C=US/ST=State/L=City/O=Organization/CN=$DOMAIN"
+  # Set appropriate file permissions
+  chmod 600 "$CERT_KEY"
+  chmod 644 "$CERT_CRT"
+  echo "Certificate generated and stored in $CERT_DIR."
+fi
 
 
-# Set appropriate file permissions
-chmod 600 "$CERT_DIR/webtrees.key"
-chmod 644 "$CERT_DIR/webtrees.crt"
