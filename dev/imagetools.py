8f49ef393d656db35c2a74236355895938a53d5b
Nathan Vaughn <nath@nvaughn.email>
2026-05-08 14:03:16 -0500

    Try to fix tags

--- dev/imagetools.py
+++ dev/imagetools.py
@@ -1,20 +1,37 @@
 import argparse
+import glob
+import json
+import os
 import subprocess
 
 from common import BASE_IMAGES, IS_GA
 
 
-def main(tags: list[str], hash: str) -> None:
+def main(tags: list[str]) -> None:
     """
     Update the image index with the given tags pointing to the given hash.
     """
+    hashes = []
+
+    # read the hashes from the downloaded build metadata files
+    for metadata_file in glob.glob(
+        os.path.join(
+            os.environ.get("GITHUB_WORKSPACE", os.getcwd()), "**", "build-metadata.json"
+        )
+    ):
+        with open(metadata_file, "r") as f:
+            metadata = json.load(f)
+            hashes.append(metadata["webtrees"]["containerimage.digest"])
+
+    # create manifest for each base image
     for base_image in BASE_IMAGES:
-        cmd = ["docker", "buildx", "imagetools", "create", "--append"]
+        cmd = ["docker", "buildx", "imagetools", "create"]
 
         for tag in [tag for tag in tags if tag.startswith(base_image)]:
             cmd.extend(["-t", tag])
 
-        cmd.append(f"{base_image}@{hash}")
+        for hash in hashes:
+            cmd.append(f"{base_image}@{hash}")
 
         print(" ".join(cmd))
 
@@ -25,7 +42,6 @@ def main(tags: list[str], hash: str) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("tags", type=str)
-    parser.add_argument("hash", type=str)
     args = parser.parse_args()
 
-    main(tags=args.tags.split(","), hash=args.hash)
+    main(tags=args.tags.split(","))
